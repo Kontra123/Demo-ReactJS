f1ac3136855a1bdd6a3f64d997586a5914fbf2b1
kontra123 <homeatone@gmail.com>
2019-12-21 00:28:07 +0200

    Updates

---
+++ precache-manifest.69244a0d581a362b2c55130a75e20378.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39db4c7657d11e61d9d6ccf68b1a4727",
+    "url": "/Demo-ReactJS/index.html"
+  },
+  {
+    "revision": "006a2cb8860fee316c72",
+    "url": "/Demo-ReactJS/static/css/main.01ca0d6e.chunk.css"
+  },
+  {
+    "revision": "3f81b913350086970c8d",
+    "url": "/Demo-ReactJS/static/js/2.9f474f12.chunk.js"
+  },
+  {
+    "revision": "006a2cb8860fee316c72",
+    "url": "/Demo-ReactJS/static/js/main.c8e826b8.chunk.js"
+  },
+  {
+    "revision": "7ff2b9f06eb03613c806",
+    "url": "/Demo-ReactJS/static/js/runtime~main.6ad7bf39.js"
+  },
+  {
+    "revision": "510613e2cc0e4e602ea8e4ab364f4b88",
+    "url": "/Demo-ReactJS/static/media/active.510613e2.svg"
+  },
+  {
+    "revision": "c2a561bee48bef633598aeaa004263ad",
+    "url": "/Demo-ReactJS/static/media/arrow-right.c2a561be.svg"
+  },
+  {
+    "revision": "aa19cc60647ba3b01c5b117b3ec2e23f",
+    "url": "/Demo-ReactJS/static/media/help-12.aa19cc60.svg"
+  }
+]);
