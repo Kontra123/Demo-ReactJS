2cdcc282202c2e97c635ab6dc960b1bb2ede68e8
kontra123 <homeatone@gmail.com>
2020-03-14 15:44:25 +0200

    chaged ui

--- src/App.js
+++ src/App.js
@@ -2,49 +2,52 @@ import React, { useEffect, useReducer } from 'react';
 import './App.css';
 import ResourceDetails from './components/resources/resourceDetails/ResourceDetails'
 import ResourceMain from './components/resources/ResourceMain.js';
-import {initResourcesMap} from './utils/resourceUtils'
+import { initResourcesMap } from './utils/resourceUtils'
 import MyProvider from './context/MyProvider'
-import mockData from './files/data.json'
+import resourcesMockData from './files/data.json'
 
 
 const App = () => {
 
-  const [state, setState] = useReducer(
-    (state, newState) => ({...state, ...newState}),
-    {currentResource: null, resources: null, actionsArray: null,  resourcesMap: null}
-  )
+    const [state, setState] = useReducer(
+        (state, newState) => ({ ...state, ...newState }),
+        { currentResource: null, resources: null, actionsArray: null, resourcesMap: null }
+    )
 
-  const handleResourceClick = (resource) => {
-    setState({currentResource: resource, actionsArray: state.resourcesMap.get(resource.id)})
-  }
+    const handleResourceClick = (resource) => {
+        setState({ currentResource: resource, actionsArray: state.resourcesMap.get(resource.id) })
+    }
 
-  useEffect(() => {
-    const runEffect = async () => {
-      const jsonFile = await mockData
+    useEffect(() => {
+        const getResources = async () => {
+            const jsonFile = await resourcesMockData
 
-      //will create map between resourceId and his actions
-      const resourcesMap = initResourcesMap(jsonFile.resources, jsonFile.actions)
+            //will create map between resourceId and his actions
+            const resourcesMap = initResourcesMap(jsonFile.resources, jsonFile.actions)
 
-      setState({currentResource: jsonFile.resources[0], resources: jsonFile.resources,
-        resourcesMap: resourcesMap, actionsArray: resourcesMap.get(jsonFile.resources[0].id)})
+            setState({
+                currentResource: jsonFile.resources[0], resources: jsonFile.resources,
+                resourcesMap: resourcesMap, actionsArray: resourcesMap.get(jsonFile.resources[0].id)
+            })
 
-    };
+        };
 
-    runEffect();
+        getResources();
 
-  }, [] );
+    }, []);
 
-  return (
-      <div className="container">
-        <h1 className="demo-header">Demo App</h1>
-        <div className="flex-row">
-         <MyProvider currentResource={state.currentResource}>
-            <ResourceMain resources={state.resources} resourceClick={handleResourceClick} />
-            <ResourceDetails actionsArray={state.actionsArray} />
-        </MyProvider>
+    return (
+        <div className="main">
+            <h1 className="demo-header">Demo App</h1>
+            <div className="flex-row">
+                <MyProvider currentResource={state.currentResource}>
+                    <ResourceMain resources={state.resources} resourceClick={handleResourceClick} />
+                    <ResourceDetails actionsArray={state.actionsArray} />
+                </MyProvider>
+            </div>
         </div>
-      </div>
-  );
+
+    );
 
 }
 
