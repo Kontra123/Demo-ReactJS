006dd674f10a34265331ee9e99a9bd011608c468
amir36 <amir@brightsource.com>
2020-08-08 22:49:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Demo-ReactJS/precache-manifest.0e5185b6e0dd6072b043beb91c3f456b.js"
+  "/Demo-ReactJS/precache-manifest.ea91ee6ddc5ebd2f517ec92e867554b5.js"
 );
 
 self.addEventListener('message', (event) => {
